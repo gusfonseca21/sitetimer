970bf22cf0b9dda9de98ea89241828206e8635bd
Gustavo <gusfonseca.dev@gmail.com>
2024-09-08 23:15:20 -0300

    Funcionalidades básicas finalizadas

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,5 @@
 import getDomain from './helper/getDomain';
+import { getTimeSpent } from './helper/getTimeSpent';
 import {
   checkDomainStored,
   checkHasFavIcon,
@@ -11,8 +12,10 @@ import { StorageData } from './types';
 
 // VARIÁVEIS GLOBAIS
 let data: StorageData = {};
+let activeDomain = '';
 let domainStartTime = 0; // Timestamp
 let domainFavIcon = '';
+let prevActiveTabId: number | null = null;
 
 // FUNÇÕES DE INICIALIZAÇÃO
 (async () => {
@@ -25,8 +28,7 @@ let domainFavIcon = '';
   }
 })();
 
-// LISTENERS
-// QUANDO UMA TAB É ATUALIZADA
+// ABRINDO NOVA ABA E INSERINDO UM URL
 chrome.tabs.onUpdated.addListener(async (_tabId, changeInfo, tab) => {
   try {
     if (tab?.favIconUrl) domainFavIcon = tab.favIconUrl;
@@ -37,6 +39,7 @@ chrome.tabs.onUpdated.addListener(async (_tabId, changeInfo, tab) => {
     */
     if (changeInfo.status === 'loading') {
       if (domain) {
+        activeDomain = domain;
         if (!(await checkDomainStored(domain))) {
           await storeNewDomain(domain);
         }
@@ -45,7 +48,7 @@ chrome.tabs.onUpdated.addListener(async (_tabId, changeInfo, tab) => {
         /*
         O favicon da página não retorna durante o status loading,
         mas retorna durante outros eventos do ciclo de vida da página
-        por isso é preciso atualizá-lo no armazenamento de dados
+        por isso é preciso atualizar os dados do banco de daos posteriormente
         */
         if (!(await checkHasFavIcon(domain)) && domainFavIcon) {
           updateDomainData(domain, null, domainFavIcon);
@@ -57,15 +60,96 @@ chrome.tabs.onUpdated.addListener(async (_tabId, changeInfo, tab) => {
   }
 });
 
-// chrome.tabs.onActivated.addListener((activeInfo) => {
-//   console.log("Tab ativada: ", activeInfo);
+// ALTERANDO A ABA ATIVA
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  console.log('activeInfo', activeInfo);
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    const domain = getDomain(tab.url);
+    if (domain) {
+      if (domain !== activeDomain) {
+        // A aba foi alterada para uma página com um domínio diferente
+        // É preciso contabilizar e salvar o tempo gasto
+        activeDomain = domain;
 
-//   chrome.tabs.get(activeInfo.tabId, (tab) => {
-//     if (tab.url) {
-//       const url = new URL(tab.url);
-//       const domain = url.hostname;
-//       console.log("Domínio capturado: ", domain);
-//     }
-//   });
+        if (prevActiveTabId) {
+          chrome.tabs.get(prevActiveTabId, async (tab) => {
+            try {
+              const prevDomain = getDomain(tab?.url);
+              if (prevDomain) {
+                let { seconds } = await getDomainData(prevDomain);
+                seconds += getTimeSpent(domainStartTime); // Somando os segundos do banco de dados com os gastos agora
+                await updateDomainData(domain, seconds);
+              }
+            } finally {
+              // As funções para atualização do banco de dados são assíncronas. Assim, o timestamp do novo domínio
+              // é atualizado e é esse valor atualizado que acaba sendo passado para a função que retorna o tempo gasto
+              // em um domínio. Foi um erro ter usado async/await. .then().catch() teria sido melhor.
+              domainStartTime = Date.now();
+            }
+          });
+        }
+      }
+    }
+
+    // Condição para quando uma aba nova é criada, quando ainda nõo há um domínio
+    if (!domain) {
+      activeDomain = '';
+      if (prevActiveTabId) {
+        chrome.tabs.get(prevActiveTabId, async (tab) => {
+          const prevDomain = getDomain(tab?.url);
+          if (prevDomain) {
+            let { seconds } = await getDomainData(prevDomain);
+            seconds += getTimeSpent(domainStartTime);
+            await updateDomainData(prevDomain, seconds);
+          }
+        });
+      }
+    }
+
+    // No final armazena dos dados da aba ativa para servir como
+    // referência quando a aba for trocada
+    prevActiveTabId = activeInfo.tabId;
+  });
+});
+
+// QUANDO A ABA ATIVA FOR FECHADA
+chrome.tabs.onRemoved.addListener((_tabId, removeInfo) => {
+  // isWindowClosing é executado quando o browser é fechado
+  // Aqui é importante que as funções sejam executadas com o método .then()
+  // pois é o que garante que os dados sejam salvos antes do navegador ser fechado completamente
+  if (removeInfo.isWindowClosing) {
+    getDomainData(activeDomain).then(({ seconds }) => {
+      return updateDomainData(
+        activeDomain,
+        (seconds += getTimeSpent(domainStartTime))
+      );
+    });
+  }
+
+  // Listener que podemos utilizar para retornar dados das abas recentemente fechadas
+  // O resultado retornado é a última aba a ser fechada
+  chrome.sessions.getRecentlyClosed({ maxResults: 1 }, async (session) => {
+    const closedTab = session[0].tab;
+    const domain = getDomain(closedTab?.url);
+
+    if (domain === activeDomain) {
+      let { seconds } = await getDomainData(domain);
+
+      seconds += getTimeSpent(domainStartTime); // Somando os segundos do banco de dados com os gastos agora
+      await updateDomainData(domain, seconds);
+    }
+  });
+});
+
+// chrome.windows.onFocusChanged.addListener(async (windowId) => {
+//   // Se windowId for -1, significa que todas as janelas perderam o foco
+//   // a contagem deve parar
+//   if (windowId === -1) {
+//     let { seconds } = await getDomainData(activeDomain);
+//     seconds += getTimeSpent(domainStartTime);
+//     await updateDomainData(activeDomain, seconds);
+//   } else {
+//     domainStartTime = Date.now();
+//   }
 // });
 
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,20 +1,58 @@
-import React, { useEffect, useState } from "react";
-import { createRoot } from "react-dom/client";
-import { clearAllData, getStoredData } from "./persistency/storage";
+import React, { useEffect, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import { clearAllData, getStoredData } from './persistency/storage';
+import { StorageData } from './types';
 
 const Popup = () => {
+  const [data, setData] = useState<StorageData>({});
+  useEffect(() => {
+    fetchStoredData();
+  }, []);
+
+  async function fetchStoredData() {
+    const storedData = await getStoredData();
+    setData(storedData);
+  }
+
   return (
-    <>
-      <button onClick={async () => console.log(await getStoredData())}>Retornar Dados</button>
+    <div style={{ width: 300 }}>
+      <div style={{ marginBottom: 10 }}>
+        {Object.keys(data).length ? (
+          <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
+            {Object.values(data).map((domain, valIndex) => {
+              return (
+                <div
+                  key={domain.favicon}
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: 10,
+                  }}
+                >
+                  <img src={domain.favicon} style={{ width: 24, height: 24 }} />
+                  <span>
+                    {`${
+                      Object.keys(data).filter(
+                        (_key, keyIndex) => keyIndex === valIndex
+                      )[0]
+                    } - ${domain.seconds} segundos`}
+                  </span>
+                </div>
+              );
+            })}
+          </div>
+        ) : null}
+      </div>
       <button onClick={clearAllData}>Limpar Dados</button>
-    </>
+    </div>
   );
 };
 
-const root = createRoot(document.getElementById("root")!);
+const root = createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
     <Popup />
   </React.StrictMode>
 );
+
